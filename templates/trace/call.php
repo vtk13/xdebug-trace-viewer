deb345a3c3a93277f033136891985baa86409920
Rogov Viktor <vtk@vda-team.com>
2014-11-06 11:15:45 +0600

    Refactor stacktrac template a bit

--- templates/trace/call.php
+++ templates/trace/call.php
@@ -15,31 +15,27 @@ use Vtk13\TraceView\Formatters\TraceHtml;
     <div class="col-md-8">
         <div class="list-group">
             <div class="list-group-item active">Stack trace for node #<?php echo $node->callId; ?></div>
-        <?php
-        $stack = new StackTrace($node);
-        $each = $node;
-        while ($each->parent) {
-            ?>
-            <div class="list-group-item">
-                <?php echo TraceHtml::nodeLine($traceName, $each); ?>
-            </div>
             <?php
-            $each = $each->parent;
-        }
-        ?>
-        </div>
-
-        <div class="list-group">
-            <div class="list-group-item active">underlying nodes of node #<?php echo $node->callId; ?></div>
-            <?php
-                foreach ($underlyingNodes as $each) {
-                    ?>
-                    <div class="list-group-item">
-                        <?php echo TraceHtml::nodeLine($traceName, $each); ?>
-                    </div>
+            $stack = new StackTrace($node);
+            $margin = 0;
+            foreach ($stack->getStraightTrace() as $each) {
+                $class = ($each == $node ? 'bg-success' : '');
+                ?>
+                <div class="list-group-item <?php echo $class; ?>" style="margin-left: <?php echo $margin; ?>px;">
+                    <?php echo TraceHtml::nodeLine($traceName, $each); ?>
+                </div>
                 <?php
-                }
+                $margin += 5;
+                $each = $each->parent;
+            }
+            foreach ($underlyingNodes as $each) {
+                ?>
+                <div class="list-group-item" style="margin-left: <?php echo $margin; ?>px;">
+                    <?php echo TraceHtml::nodeLine($traceName, $each); ?>
+                </div>
+                <?php
+            }
             ?>
         </div>
-</div>
+    </div>
 </div>
